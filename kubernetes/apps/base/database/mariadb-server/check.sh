f2bf7ecc86f3ae5c2242b813fe969419e3775f3e
Sascha Stroinski <sascha@stroinski.info>
2026-03-23 17:45:01 +0100

    update

--- kubernetes/apps/base/database/mariadb-server/check.sh
+++ kubernetes/apps/base/database/mariadb-server/check.sh
@@ -4,4 +4,4 @@ password_aux="${MYSQL_ROOT_PASSWORD:-}"
 if [[ -f "${MYSQL_ROOT_PASSWORD_FILE:-}" ]]; then
     password_aux=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
 fi
-mysqladmin status -uroot -p"${password_aux}"
+mariadb-admin status -uroot -p"${password_aux}"
