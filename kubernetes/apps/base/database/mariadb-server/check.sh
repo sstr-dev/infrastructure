f0d5083d18f412a2c0b6ab97620af11af21aa563
Sascha Stroinski <sascha@stroinski.info>
2026-03-23 18:12:07 +0100

    check

--- kubernetes/apps/base/database/mariadb-server/check.sh
+++ kubernetes/apps/base/database/mariadb-server/check.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -ec
 
-password_aux="${MYSQL_ROOT_PASSWORD:-}"
-if [[ -f "${MYSQL_ROOT_PASSWORD_FILE:-}" ]]; then
-    password_aux=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+password_args=()
+if [[ "${MARIADB_HEALTHCHECK_USE_PASSWORD:-false}" == "true" ]]; then
+    password_aux="${MYSQL_ROOT_PASSWORD:-}"
+    if [[ -f "${MYSQL_ROOT_PASSWORD_FILE:-}" ]]; then
+        password_aux=$(cat "$MYSQL_ROOT_PASSWORD_FILE")
+    fi
+    password_args=(-p"${password_aux}")
 fi
-mariadb-admin status -uroot -p"${password_aux}"
+
+mariadb-admin status -uroot "${password_args[@]}"
